96d8b3f635770745fd25f65d86d8829a4e845540
aqua <aquaform@yandex.ru>
2021-03-01 11:03:57 +0500

    add auth and category properties

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client"
 
 export default function Home() {
+  const [session] = useSession()  
+
   return (
     <>
       <Head>
@@ -8,8 +11,18 @@ export default function Home() {
       </Head>
 
       <nav className="authNew">
-        <button>Sign In</button>
-        <button>Sign Out</button> 
+        {!session ? (
+          <button onClick={signIn}>Sign In</button> 
+          ) : ( 
+          <>
+            <span>{session.user.name}</span>
+            {session.user.image && (
+              <img src={session.user.image} style={{width: "25px", borderRadius: "50%"}}/>
+            )}
+            <button onClick={signOut}>Sign Out</button>
+          </>
+          
+        )}          
       </nav>      
     </>
   )
--- prisma/migrations/20210227122305_init/migration.sql
+++ prisma/migrations/20210227122305_init/migration.sql
@@ -1,15 +1,67 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `phone` VARCHAR(191) NOT NULL,
-    `username` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191),
+    `email` VARCHAR(191),
+    `email_verified` DATETIME(3),
+    `image` VARCHAR(191),
+    `phone` VARCHAR(191),
+    `fistname` VARCHAR(191),
     `surname` VARCHAR(191),
-    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+    `role` ENUM('USER', 'ADMIN') DEFAULT 'USER',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 UNIQUE INDEX `users.email_unique`(`email`),
 UNIQUE INDEX `users.phone_unique`(`phone`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `compound_id` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `provider_type` VARCHAR(191) NOT NULL,
+    `provider_id` VARCHAR(191) NOT NULL,
+    `provider_account_id` VARCHAR(191) NOT NULL,
+    `refresh_token` VARCHAR(191),
+    `access_token` VARCHAR(191),
+    `access_token_expires` DATETIME(3),
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+UNIQUE INDEX `accounts.compound_id_unique`(`compound_id`),
+INDEX `providerAccountId`(`provider_account_id`),
+INDEX `providerId`(`provider_id`),
+INDEX `userId`(`user_id`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sessions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `session_token` VARCHAR(191) NOT NULL,
+    `access_token` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+UNIQUE INDEX `sessions.session_token_unique`(`session_token`),
+UNIQUE INDEX `sessions.access_token_unique`(`access_token`),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `verification_requests` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+UNIQUE INDEX `verification_requests.token_unique`(`token`),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -44,7 +96,7 @@ CREATE TABLE `tags` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
-    `categoryId` INTEGER NOT NULL,
+    `category_id` INTEGER,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -54,18 +106,18 @@ CREATE TABLE `feedbacks` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `feedback` VARCHAR(191) NOT NULL,
     `rate` DOUBLE NOT NULL,
-    `authorId` INTEGER NOT NULL,
-    `placeId` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `place_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `likes` (
-    `authorId` INTEGER NOT NULL,
-    `placeId` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `place_id` INTEGER NOT NULL,
 
-    PRIMARY KEY (`authorId`,`placeId`)
+    PRIMARY KEY (`user_id`,`place_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -74,7 +126,7 @@ CREATE TABLE `parties` (
     `title` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `date` DATETIME(3) NOT NULL,
-    `placeId` INTEGER NOT NULL,
+    `place_id` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -84,7 +136,7 @@ CREATE TABLE `picturies` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `href` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191),
-    `placeId` INTEGER NOT NULL,
+    `place_id` INTEGER,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -106,25 +158,25 @@ INDEX `_PlaceToTag_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `tags` ADD FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tags` ADD FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `feedbacks` ADD FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `feedbacks` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `feedbacks` ADD FOREIGN KEY (`placeId`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `feedbacks` ADD FOREIGN KEY (`place_id`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `likes` ADD FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `likes` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `likes` ADD FOREIGN KEY (`placeId`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `likes` ADD FOREIGN KEY (`place_id`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `parties` ADD FOREIGN KEY (`placeId`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `parties` ADD FOREIGN KEY (`place_id`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `picturies` ADD FOREIGN KEY (`placeId`) REFERENCES `places`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `picturies` ADD FOREIGN KEY (`place_id`) REFERENCES `places`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_CategoryToPlace` ADD FOREIGN KEY (`A`) REFERENCES `categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
